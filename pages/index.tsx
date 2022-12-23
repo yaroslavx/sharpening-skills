56a484a5fbdf946c5ef05489d7a5e92fcda9bc7c
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-12-23 23:17:08 +0300

    setup Next

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>App For Booking</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='bg-slate-200'>Hi there</div>
+    </>
+  )
+}
